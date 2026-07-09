b099e456ec995b0b550a8941b00335c04c8f12a7
mingyo77 <wells7845@naver.com>
2026-07-09 17:11:03 +0900

    feat: add ui foundation tokens
    # Conflicts:
    #	android/app/src/main/java/com/example/itday/ui/theme/Color.kt

---
+++ android/app/src/main/java/com/example/itday/ui/theme/Shape.kt
@@ -0,0 +1,9 @@
+package com.example.itday.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object ItDayRadius {
+    val ButtonRadius = 20.dp
+    val ButtonRadiusSmall = 10.dp
+    val CardRadius = 24.dp
+}
