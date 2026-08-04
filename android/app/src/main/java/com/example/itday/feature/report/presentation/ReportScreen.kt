46772eb41cc79c598bf5bd7d1615e6b64ed38db5
mingyo77 <wells7845@naver.com>
2026-08-04 22:07:38 +0900

    feat: 출석 체크 콘텐츠 UI 구현

--- android/app/src/main/java/com/example/itday/feature/report/presentation/ReportScreen.kt
+++ android/app/src/main/java/com/example/itday/feature/report/presentation/ReportScreen.kt
@@ -5,7 +5,7 @@ import com.example.itday.R
 import com.example.itday.feature.report.presentation.component.ReportCategoryItem
 import com.example.itday.feature.report.presentation.content.GuestReportContent
 import com.example.itday.feature.report.presentation.content.FreeReportContent
-import com.example.itday.feature.report.presentation.content.PointHistoryContent
+import com.example.itday.feature.report.presentation.content.AttendanceContent
 
 @Composable
 fun ReportScreen(
@@ -33,6 +33,6 @@ fun ReportScreen(
                 onShopClick = onShopClick,
                 onPointHistoryClick = onPointHistoryClick,
             )
-        ReportAccess.Pro -> PointHistoryContent()
+        ReportAccess.Pro -> AttendanceContent()
     }
 }
