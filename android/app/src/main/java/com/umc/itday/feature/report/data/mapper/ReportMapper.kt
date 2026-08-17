368c23da352e183a4527523ff599b098e8faf8e1
mingyo77 <wells7845@naver.com>
2026-08-17 19:04:28 +0900

    feat: 출석 및 방문 도전과제 API 연동

--- android/app/src/main/java/com/umc/itday/feature/report/data/mapper/ReportMapper.kt
+++ android/app/src/main/java/com/umc/itday/feature/report/data/mapper/ReportMapper.kt
@@ -1,12 +1,44 @@
 package com.umc.itday.feature.report.data.mapper
 
 import com.umc.itday.feature.report.data.model.CreateReportRequestDto
+import com.umc.itday.feature.report.data.model.AttendanceDto
 import com.umc.itday.feature.report.data.model.ReportDetailDto
 import com.umc.itday.feature.report.data.model.ReportSummaryDto
+import com.umc.itday.feature.report.data.model.VisitChallengeDto
+import com.umc.itday.feature.report.domain.model.Attendance
 import com.umc.itday.feature.report.domain.model.CreateReportInput
 import com.umc.itday.feature.report.domain.model.ReportDetail
 import com.umc.itday.feature.report.domain.model.ReportPeriodType
 import com.umc.itday.feature.report.domain.model.ReportSummary
+import com.umc.itday.feature.report.domain.model.VisitChallenge
+
+fun AttendanceDto.toDomain() =
+    Attendance(
+        attendanceId = attendanceId,
+        attendanceDate = attendanceDate,
+        consecutiveDays = consecutiveDays,
+        dailyPoint = dailyPoint,
+        bonusPoint = bonusPoint,
+        earnedPoint = earnedPoint,
+        monthlyAttendanceCount = monthlyAttendanceCount,
+        monthlyEarnedPoint = monthlyEarnedPoint,
+        sevenDayBonusReceived = sevenDayBonusReceived,
+        fifteenDayBonusReceived = fifteenDayBonusReceived,
+        thirtyDayBonusReceived = thirtyDayBonusReceived,
+    )
+
+fun VisitChallengeDto.toDomain() =
+    VisitChallenge(
+        visitCount = visitCount,
+        currentStage = currentStage,
+        nextTargetVisitCount = nextTargetVisitCount,
+        currentStageRewardPoint = currentStageRewardPoint,
+        accumulatedRewardPoint = accumulatedRewardPoint,
+        newlyEarnedPoint = newlyEarnedPoint,
+        progressRate = progressRate,
+        completed = completed,
+        characterVisual = characterVisual,
+    )
 
 fun ReportSummaryDto.toDomain() =
     ReportSummary(
